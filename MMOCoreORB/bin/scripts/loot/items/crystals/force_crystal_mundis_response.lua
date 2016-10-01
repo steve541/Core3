85bde94fd400f2bc81e332f833badf97968fb016
steve541 <stevenc.combs@yahoo.com>
2016-10-01 18:44:30 -0500

    Update and rename MMOCoreORB/bin/scripts/loot/items/force_crystal_mundis_response.lua to MMOCoreORB/bin/scripts/loot/items/crystals/force_crystal_mundis_response.lua

--- MMOCoreORB/bin/scripts/loot/items/crystals/force_crystal_mundis_response.lua
+++ MMOCoreORB/bin/scripts/loot/items/crystals/force_crystal_mundis_response.lua
@@ -12,7 +12,7 @@ force_crystal_mundis_response = {
 		{"attackhealthcost",0,5,0},
 		{"attackactioncost",0,5,0},
 		{"attackmindcost",0,5,0},
-		{"forcecost",0,4,0},
+		{"forcecost",0,9.9,0},
 		{"color",31,31,0},
 		{"quality",6,6,0},
 	},
