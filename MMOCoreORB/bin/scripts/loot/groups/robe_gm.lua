d20c43881bc1a872b8c84331fff3862d1ca78211
steve541 <stevenc.combs@yahoo.com>
2016-09-30 23:06:26 -0500

    Create robe_gm.lua

---
+++ MMOCoreORB/bin/scripts/loot/groups/robe_gm.lua
@@ -0,0 +1,10 @@
+robe_gm = {
+        description = "",
+        minimumLevel = 0,
+        maximumLevel = 0,
+        lootItems = {
+                {itemTemplate = "gm_robe", weight = 10000000},
+        }
+}
+
+addLootGroupTemplate("robe_gm", robe_gm)
